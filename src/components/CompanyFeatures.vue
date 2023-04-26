fb676e56b1e342fc2144b49d6880eb8ee3818cbb
nespolisimmaco <snespoli@yahoo.com>
2023-04-26 21:58:26 +0200

    Add: CompanyFeatures component implemented

--- src/components/CompanyFeatures.vue
+++ src/components/CompanyFeatures.vue
@@ -17,16 +17,16 @@ export default {
                     description: "Punctuality is our top priority because it's an essential criteria to assess a program quality."
                 },
                 {
-                    title: "One to One",
-                    description: ""
+                    title: "Online Courses",
+                    description: "Online business coaching now offers you a very powerful way to empower your business into success."
                 },
                 {
-                    title: "One to One",
-                    description: "Getting the necessary clarity about the current state to help ypu improve your game"
+                    title: "Consulting",
+                    description: "You will get a clear sense of direction for your business, thorough assessment and faster results."
                 },
                 {
-                    title: "One to One",
-                    description: "Getting the necessary clarity about the current state to help ypu improve your game"
+                    title: "Self Development",
+                    description: "Business Coaching often kep your focus and develop you both in a professional and personal way."
                 },
             ]
         }
@@ -42,10 +42,23 @@ export default {
             <h5>Let's Dream Big Together</h5>
             <h2>We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply
                 executive coaching in the new age of digitalization.</h2>
+            <!-- Container fluid -->
             <div class="container-fluid">
-                <div class="row row-cols-3">
-
+                <!-- Row -->
+                <div class="row row-cols-3 g-5">
+                    <!-- Column -->
+                    <div class="col text-start" v-for="(feature, index) in features" :key="index">
+                        <h4>{{ feature.title }}</h4>
+                        <p>{{ feature.description }}</p>
+                    </div>
+                    <!-- End Column -->
                 </div>
+                <!-- End Row -->
+            </div>
+            <!-- End Container fluid -->
+            <div class="buttons mt-5">
+                <button class="btn white-button">Learn more</button>
+                <button class="btn green-button">Get started today</button>
             </div>
         </div>
         <!-- End Container -->
@@ -58,7 +71,43 @@ export default {
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
 
+.features {
+    padding: 5rem 0 4rem;
+}
+
 .ms_container {
     @include ms_container();
+
+    h5 {
+        color: $main-green;
+    }
+
+    h2 {
+        margin: 2rem 0 3rem;
+    }
+}
+
+.col {
+    p {
+        font-size: .9rem;
+    }
+}
+
+.white-button {
+    color: $main-green;
+    font-weight: 600;
+    font-size: .8rem;
+    border: 1px solid lightgrey;
+    padding: 10px 2rem;
+    margin-right: 10px;
+}
+
+.green-button {
+    background-color: $main-green;
+    font-weight: 600;
+    font-size: .8rem;
+    padding: 10px 2rem;
+    color: white;
+    margin-left: 10px;
 }
 </style>
