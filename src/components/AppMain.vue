60cb1604514275dba7307bfccd0d41e1e0f62ae8
nespolisimmaco <snespoli@yahoo.com>
2023-04-27 16:49:06 +0200

    Add: video poster with youtube icon created

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,7 +14,6 @@ export default {
         QA
     }
 }
-}
 </script>
         
 <template>
--- src/components/QA.vue
+++ src/components/QA.vue
@@ -12,14 +12,22 @@ export default {
         <!-- Container -->
         <div class="container d-flex justify-content-center">
             <!-- Video -->
-            <div class="video me-5">
-                <img src="../assets/img/home-business-video-poster-670x450.jpg" alt="Video poster">
+            <div class="video d-flex align-items-center me-5">
+                <img class="video-poster" src="../assets/img/home-business-video-poster-670x450.jpg" alt="Video poster">
+                <img class="youtube-icon" src="../assets/img/icon-youtube-play.png" alt="Youtube icon">
             </div>
             <!-- Question and answers -->
             <div class="questions ms-5">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus expedita cum itaque esse.
-                    Eaque voluptas accusantium eveniet quia quam ducimus illum fugit laborum, fuga excepturi facilis
-                    sed, repudiandae dolor commodi?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam sint sequi tempore deleniti
+                    illum consequatur. Esse in blanditiis atque quas sunt facilis, recusandae illum, vitae, totam id earum
+                    aut sit. Doloremque earum est at inventore eius, ratione odit quisquam molestias. Ut sequi, obcaecati ea
+                    eligendi impedit, ipsa quo eos atque cupiditate esse, nostrum ipsum ipsam similique eius ab dolorum est
+                    rem aut saepe eaque laboriosam? Vero magni minima quis explicabo laboriosam laudantium quasi. Ab a est
+                    consectetur maxime ullam dolor nihil temporibus itaque. Molestias illum velit reprehenderit incidunt
+                    exercitationem optio, nesciunt, culpa repudiandae explicabo officia veritatis debitis vel asperiores.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione sequi eos non quos accusamus
+                    eaque explicabo esse alias ducimus.
+                </p>
             </div>
         </div>
         <!-- End Container -->
@@ -36,14 +44,23 @@ export default {
     padding: 5rem 0;
 
     .video {
-        img {
-            height: 300px;
+        position: relative;
+
+        .video-poster {
+            height: 350px;
+        }
+
+        .youtube-icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
         }
     }
 
     .questions {
         color: white;
-        width: 35%;
+        width: 30%;
     }
 }
 </style>
