5846db398493ed41c2e289b9a010c7c985b195f7
nespolisimmaco <snespoli@yahoo.com>
2023-04-27 11:41:13 +0200

    Add: moved img folder to assets folder

--- src/components/CompanyServices.vue
+++ src/components/CompanyServices.vue
@@ -5,16 +5,19 @@ export default {
         return {
             services: [
                 {
+                    image: "home-business-service-slide-01-480x298.jpg",
                     title: "Consultative Training",
-                    paragraph: "Lorem ipsum"
+                    paragraph: "With a coach adrressing multiple issues that are off balance, affecting your business in various unconsidered ways."
                 },
                 {
+                    image: "home-business-service-slide-02-480x298.jpg",
                     title: "Real Deal Coaching",
-                    paragraph: "Lorem ipsum"
+                    paragraph: "Brings an exceptionally powerful opportunity. Being able to accept, to work with that oppurtunity."
                 },
                 {
+                    image: "home-business-service-slide-03-480x298.jpg",
                     title: "Advisor Training Program",
-                    paragraph: "Lorem ipsum"
+                    paragraph: "Getting high quality, entrepreneur mindset driven online business coaching, is what is needed."
                 },
             ]
         }
@@ -33,7 +36,22 @@ export default {
                 <h2>Explore How can I help you</h2>
                 <!-- Row -->
                 <div class="row row-cols-3">
-
+                    <!-- Column -->
+                    <div class="col" v-for="(service, index) in services">
+                        <!-- Card -->
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ service.title }}</h5>
+                                <p class="card-text">{{ service.paragraph }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#">Discover now &rarr;</a>
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                    </div>
+                    <!-- Column -->
                 </div>
                 <!-- Row -->
             </div>
@@ -55,20 +73,35 @@ export default {
 
 .services {
     background-color: $jumbotron-background;
+    padding: 4rem 0;
 
     h5 {
         color: $main-green;
     }
 
     h2 {
-        font-weight: 600
+        font-weight: 600;
+    }
+}
+
+.card-body {
+    h5 {
+        color: black;
+    }
+
+    p {
+        font-size: .8rem;
     }
 }
 
 .card-footer {
+    background-color: white;
+
     a {
         text-decoration: none;
         color: $main-green;
+        font-size: .8rem;
+        font-weight: 700;
     }
 }
 </style>
